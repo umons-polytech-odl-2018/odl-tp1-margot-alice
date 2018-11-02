394961ade2e1cedc9911cd32fed3f5c53583fb0c
AliceTondeur <Alice.TONDEUR@student.umons.ac.be>
2018-11-02 15:32:56 +0100

    exo 1 et 2

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,28 @@
 package exercise1;
+class Person{
 
-class Person {
+	private String name;
+	private int age;
+
+	public Person(){
+
+		name="default value";
+		age= 123;
+
+	}
+	public Person(String name, int age){
+
+		this.name=name;
+		this.age=age;
+
+	}
+	public String getName(){
+		return name ;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
+
 }
--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,26 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int populationsize=0;
+	private static int totalAge=0;
+
+	public Person(int age){
+		this.age=age;
+		populationsize++;
+		totalAge+=age;
+	}
+
+	public static int computePopulationSize(){
+		return populationsize;
+	}
+	public static float computeAveragePopulationAge(){
+		return((float)totalAge/(float)populationsize);
+	}
+	public static void resetPopulation(){
+		populationsize=0;
+		totalAge=0;
+	}
 }
+
+
